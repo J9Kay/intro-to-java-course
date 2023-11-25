400ef01940ce975e22d58774305d7aecf24e6c9a
J9Kay <janine.kayode@gmail.com>
2023-11-25 11:57:37 +0000

    Start shape exercise

---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/shapes/Shape.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.shapes;
+
+public abstract class Shape {
+    String name;
+
+    abstract double getArea();
+    
+    String getName() {
+        return name;
+    }
+
+
+}
---
+++ exercises/object-oriented-programming/src/main/java/com/cbfacademy/shapes/Sphere.java
@@ -0,0 +1,14 @@
+package com.cbfacademy.shapes;
+
+public class Sphere extends Shape {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 4 * Math.PI * radius * radius;
+    }
+}
