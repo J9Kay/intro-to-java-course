26283b2b8aaba8ae83e40f1d83b0c9f67afdf522
“J9Kay” <134739366+J9Kay@users.noreply.github.com>
2023-11-29 21:33:32 +0000

    Start exercise part 1

---
+++ exercises/input-output/src/main/java/com/cbfacademy/FileManager.java
@@ -0,0 +1,30 @@
+package com.cbfacademy;
+
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import static java.nio.file.StandardCopyOption.*;
+
+public class FileManager {
+    String filename;
+    String inputFile;
+    String outputFile;
+ 
+    
+    public List<String> readFile(String filename) {
+        List<String> file_list = new ArrayList<>();
+        
+        return file_list;
+    }
+    // append reading of each line and append to the array.
+    //    private void copyFileUsingStream(String inputFile2, String outputFile2) {
+    // }
+
+    public void copy(String inputFile, String outputFile) {
+        copy(inputFile, outputFile);
+
+
+    }
+
+//  read the file and write the file to the output file
+}
