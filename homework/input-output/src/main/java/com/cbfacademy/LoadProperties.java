80c43b163322b52431125b0c93ec075fa364e300
J9Kay <janine.kayode@gmail.com>
2023-12-09 23:53:32 +0000

    Work on input output

--- homework/input-output/src/main/java/com/cbfacademy/LoadProperties.java
+++ homework/input-output/src/main/java/com/cbfacademy/LoadProperties.java
@@ -1,5 +1,6 @@
-package com.cbfacademy; 
+package com.cbfacademy;
 
+ 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -7,9 +8,13 @@ import java.util.Properties;
 public class LoadProperties {
     public static void main(String[] args) {
         Properties prop = new Properties();
+
         try (FileInputStream input = new FileInputStream("example.properties")) {
+            // Load properties file
             prop.load(input);
-            prop.forEach((key, value) -> System.out.println(key + " = " + value));
+
+            // Iterate over properties and print them
+            prop.forEach((key, value) -> System.out.println(key + ": " + value));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
