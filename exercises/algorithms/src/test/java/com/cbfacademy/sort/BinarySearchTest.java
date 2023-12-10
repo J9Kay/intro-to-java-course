f663d7d086c8e00e60fd247b07db8949303e0dd4
J9Kay <janine.kayode@gmail.com>
2023-12-10 19:05:01 +0000

    Update excercise

--- exercises/algorithms/src/test/java/com/cbfacademy/sort/BinarySearchTest.java
+++ exercises/algorithms/src/test/java/com/cbfacademy/sort/BinarySearchTest.java
@@ -1,6 +1,5 @@
 package com.cbfacademy.sort;
 
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +9,13 @@ class BinarySearchTest {
     void testBinarySearch() {
         BinarySearch binarySearch = new BinarySearch();
         int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        assertEquals(4, binarySearch.search(arr, 5)); // Index of 5 in the array
-        assertEquals(-1, binarySearch.search(arr, 10)); // 10 is not in the array
+
+        int result = binarySearch.search(arr, 5);
+        assertEquals(4, result);
+        System.out.println("Test for value 5 passed. Expected index: 4, Found index: " + result);
+
+        result = binarySearch.search(arr, 10);
+        assertEquals(-1, result);
+        System.out.println("Test for value 10 passed. Expected index: -1 (not found), Found index: " + result);
     }
 }
