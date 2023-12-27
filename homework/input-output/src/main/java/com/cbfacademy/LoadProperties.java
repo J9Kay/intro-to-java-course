434d66b1effc83c4c6e244f0d6c5ae07e1f9055d
J9Kay <janine.kayode@gmail.com>
2023-12-27 21:28:50 +0000

    Update homework

--- homework/input-output/src/main/java/com/cbfacademy/LoadProperties.java
+++ homework/input-output/src/main/java/com/cbfacademy/LoadProperties.java
@@ -1,15 +1,17 @@
 package com.cbfacademy;
 
- 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class LoadProperties {
     public static void main(String[] args) {
         Properties prop = new Properties();
 
-        try (FileInputStream input = new FileInputStream("example.properties")) {
+        try (InputStream input = LoadProperties.class.getClassLoader().getResourceAsStream("example.properties")) {
+            if (input == null) {
+                throw new IOException("Unable to find example.properties");
+            }
             // Load properties file
             prop.load(input);
 
@@ -20,4 +22,3 @@ public class LoadProperties {
         }
     }
 }
-
